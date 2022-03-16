77efc0d8335663f42a75fda0fdbd075f7459911b
user <user@A401-PC50>
2022-03-16 16:12:15 +0800

    加入專案檔案。

---
+++ 20220316/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220316
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入你的分數:");
+            score = Convert.ToInt32(Console.ReadLine());
+            string resuit = score >= 60 ? "及格" : "不及格";
+            Console.WriteLine("同學你好，這是你(0)", resuit);
+            Console.ReadLine();
+        }
+    }
+}
